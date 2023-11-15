0b1e667c7d4bc38ba997889d7cc847a3e72aa289
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2023-11-15 14:23:33 -0500

    new code

---
+++ code.py
@@ -0,0 +1,21 @@
+##!/usr/bin/env python3
+
+"""
+Created by: Justin, Lavoie
+Created on: Oct, 2023
+This module uses pybadge
+"""
+
+import stage
+import ugame
+
+def game_scene() -> None:
+"""This function is the main game game_scene"""
+    print ("\n\n\n")
+    print("Hello World")
+    
+    while True:
+        pass
+    
+if __name__ == "__main__":
+    game_scene()
