aba4163cb5b114c3200bd463f54a587367273417
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2023-11-26 17:55:01 -0500

    learning guide 3 & 4

---
+++ Learning_Guide_03/code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Created by: Justin, Lavoie
+Created on: Nov, 2023
+This program is the "Space Aliens" program on the PyBadge
+"""
+
+
+import stage
+import ugame
+
+
+def game_scene() -> None:
+    """
+    This function is the main game game_scene
+    """
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [ship] + [background]
+    game.render_block()
+    while True:
+        game.render_sprites([ship])
+        game.tick()
+
+if __name__ == "__main__":
+    game_scene()
