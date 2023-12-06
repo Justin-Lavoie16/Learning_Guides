0ebfa7c1646dbb5cfe42f314b78c725ec8ba1d19
Justin Lavoie <113055699+Justin-Lavoie16@users.noreply.github.com>
2023-12-06 10:56:20 -0500

    fixed audio bug

--- Learning_Guide_07/code.py
+++ Learning_Guide_07/code.py
@@ -100,15 +100,15 @@ def game_scene() -> None:
         # get user input
         keys = ugame.buttons.get_pressed()
         if keys & ugame.K_X != 0:
-                if a_button == constants.button_state["button_up"]:
-                    a_button = constants.button_state["button_just_pressed"]
-                elif a_button == constants.button_state["button_just_pressed"]:
-                    a_button = constants.button_state["button_still_pressed"]
-                    if a_button == constants.button_state["button_still_pressed"]:
-                        a_button = constants.button_state["button_released"]
-                else:
-                    a_button = constants.button_state["button_up"]
-
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
         if keys & ugame.K_RIGHT != 0:
             if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
                 ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
